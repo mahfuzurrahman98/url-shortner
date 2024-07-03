93a2f58b28aaf434aa78d3ed7feb8763e555fc4d
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2024-07-03 17:47:41 +0600

    service to compare two url

---
+++ server/app/Services/UrlComparisonService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+class UrlComparisonService {
+    public function areUrlsEquivalent(string $url1, string $url2): bool {
+        // Normalize URLs
+        $normalizedUrl1 = $this->normalizeUrl($url1);
+        $normalizedUrl2 = $this->normalizeUrl($url2);
+
+        // Compare normalized URLs
+        return $normalizedUrl1 === $normalizedUrl2;
+    }
+
+    public function normalizeUrl(string $url): string {
+        // Remove scheme (http, https)
+        $url = preg_replace('#^https?://#', '', $url);
+
+        // Remove www subdomain
+        $url = preg_replace('#^www\.#', '', $url);
+
+        // Remove trailing slashes
+        $url = rtrim($url, '/');
+
+        return $url;
+    }
+}
