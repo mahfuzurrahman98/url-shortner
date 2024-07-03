9c109ca94ef6fbe1c690abda480bf783dc83de01
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2024-07-03 17:03:00 +0600

    first commit

---
+++ client/src/views/RedirectorView.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>{{ hash }}</h1>
+</template>
+
+<script setup>
+    import { useRoute } from 'vue-router';
+    const route = useRoute();
+
+    const hash = route.params.hash;
+</script>
