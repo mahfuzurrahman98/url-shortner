891283f116143f942404ec2d70d506b1ca8f556b
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-07-04 10:16:13 +0600

    service class for creating short unique url

---
+++ server/app/Services/URLShortenerService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Shorten;
+
+class URLShortenerService {
+    protected $length = 6;
+    protected $charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    public function generateUniqueHash(): string {
+        $hash = $this->generateHash();
+
+        if ($this->hashExists($hash)) {
+            return $this->generateUniqueHash(); // Recursive call if hash exists
+        }
+
+        return $hash;
+    }
+
+    private function generateHash(): string {
+        $hash = '';
+        $charsetLength = strlen($this->charset);
+        for ($i = 0; $i < $this->length; $i++) {
+            $randomIndex = rand(0, $charsetLength - 1);
+            $hash .= $this->charset[$randomIndex];
+        }
+        return $hash;
+    }
+
+    private function hashExists(string $hash): bool {
+        return Shorten::where('short_url', $hash)->exists();
+    }
+}
