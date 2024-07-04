234134d33dc76be1bc19ab85d121b3eb94a0a4ba
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-07-05 01:52:55 +0600

    improved the 404 component

--- client/src/components/NotFound.vue
+++ client/src/components/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>404</h1>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+        <div class="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
+            <div class="text-lg text-gray-700">
+                <p>404 | The page you are looking for does not exist.</p>
+            </div>
+        </div>
+    </div>
 </template>
