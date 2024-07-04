386418cc86248161ac16ab2ff51271b60954a4bd
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-07-04 10:10:37 +0600

    the controller is almost finalized

--- server/app/Http/Controllers/ShortenController.php
+++ server/app/Http/Controllers/ShortenController.php
@@ -4,66 +4,102 @@ namespace App\Http\Controllers;
 
 use App\Models\Shorten;
 use Illuminate\Http\Request;
-use App\Services\UrlSafetyService;
-use App\Services\UrlComparisonService;
+use App\Services\URLService;
+use App\Services\URLShortenerService;
 use Illuminate\Support\Facades\Validator;
 
 class ShortenController extends Controller {
-    protected $urlSafetyService;
-    protected $urlCompressionService;
+    protected $urlService;
+    protected $urlShortenerService;
 
     public function __construct(
-        UrlSafetyService $urlSafetyService,
-        UrlComparisonService $urlCompressionService
+        URLService $urlSerive,
+        URLShortenerService $urlShortenerService
     ) {
-        $this->urlSafetyService = $urlSafetyService;
-        $this->urlCompressionService = $urlCompressionService;
+        $this->urlService = $urlSerive;
+        $this->urlShortenerService = $urlShortenerService;
     }
 
     public function store(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'original_url' => 'required|url',
-        ]);
+        try {
+            // Validation
+            $validator = Validator::make($request->all(), [
+                'originalUrl' => 'required|url',
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => false,
+                    'message' => $validator->errors()->first()
+                ], 422);
+            }
 
-        if ($validator->fails()) {
+            $originalUrl = $request->originalUrl;
+            $normalizedUrl = $this->urlService->normalizeUrl($originalUrl);
+
+            // Check if the URL already exists in the database
+            $existingUrl = Shorten::where('normalized_url', $normalizedUrl)->first();
+            if ($existingUrl) {
+                return response()->json([
+                    'success' => true,
+                    'data' => [
+                        'shortUrl' => $existingUrl->short_url
+                    ]
+                ], 200);
+            }
+
+            // Check URL safety
+            if (!$this->urlService->isUrlSafe($originalUrl)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'The URL is not safe.'
+                ], 400);
+            }
+
+            // Generate short URL logic
+            $shortUrl = $this->urlShortenerService->generateUniqueHash($originalUrl);
+
+            // Store URL in database
+            $newUrl = Shorten::create([
+                'original_url' => $originalUrl,
+                'normalized_url' => $normalizedUrl,
+                'short_url' => $shortUrl,
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'data' => [
+                    'shortUrl' => $newUrl->short_url
+                ]
+            ], 200);
+        } catch (\Exception $e) {
             return response()->json([
                 'success' => false,
-                'message' => $validator->errors()->first()
+                'message' => $e->getMessage()
             ], 400);
         }
-
-        $original_url = $request->original_url;
-        $normalized_url = $this->urlCompressionService->normalizeUrl($original_url);
-
-
-        // Check if the URL already exists in the database
-        $existingUrl = Shorten::where('normalized_url', $normalized_url)->first();
-        if ($existingUrl) {
-            return response()->json(['short_url' => url($existingUrl->short_url)]);
-        }
-
-
-        // Check URL safety
-        if (!$this->urlSafetyService->isUrlSafe($original_url)) {
-            return response()->json(['error' => 'URL is not safe'], 400);
-        }
-
-
-
-
-        // Generate short URL logic
-        $shortUrl = $this->generateShortUrl();
-
-        // Store URL in database
-        $newUrl = Shorten::create([
-            'original_url' => $original_url,
-            'normalized_url' => $normalized_url,
-            'short_url' => $shortUrl,
-        ]);
-
-        return response()->json(['short_url' => url($newUrl->short_url)]);
     }
 
-    public function show(Request $request) {
+    public function show(Request $request, string $hash) {
+        try {
+            $url = Shorten::where('short_url', $hash)->first();
+            if ($url) {
+                return response()->json([
+                    'success' => true,
+                    'data' => [
+                        'originalUrl' => $url->original_url
+                    ]
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'The URL does not exist.'
+                ], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 }
