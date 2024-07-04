be24d401ac17bcb076424e1cfe6c1ff874076df3
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-07-04 10:10:14 +0600

    declare mass assignable columns

--- server/app/Models/Shorten.php
+++ server/app/Models/Shorten.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Shorten extends Model
-{
+class Shorten extends Model {
     use HasFactory;
+
+    protected $fillable = ['original_url', 'normalized_url',  'short_url'];
 }
