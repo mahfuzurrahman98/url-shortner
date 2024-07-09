026045327baff16a1ad8649f1ab125fc22061f67
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-07-09 23:46:32 +0600

    add folder functionality to the component

--- client/src/views/RedirectorView.vue
+++ client/src/views/RedirectorView.vue
@@ -25,15 +25,20 @@
     import axios from '../api/axios';
     import { onMounted, ref } from 'vue';
 
-    const route = useRoute();
-    const hash = route.params.hash;
     const errorMessage = ref(null);
 
-    const getUrl = async () => {
+    const getUrl = async (folder, hash) => {
         try {
-            const response = await axios.get(`/shortens/${hash}`);
-            if (response.data.success) {
-                window.location.href = response.data.data.originalUrl;
+            if (folder) {
+                const response = await axios.get(`/shortens/${folder}/${hash}`);
+                if (response.data.success) {
+                    window.location.href = response.data.data.originalUrl;
+                }
+            } else {
+                const response = await axios.get(`/shortens/${hash}`);
+                if (response.data.success) {
+                    window.location.href = response.data.data.originalUrl;
+                }
             }
         } catch (error) {
             // console.error(error);
@@ -42,8 +47,44 @@
         }
     };
 
+    const isFolderValid = (folder) => {
+        // if anything rather than a-z, 0-9, - or _ then return false
+        if (!folder.match(/^[a-zA-Z0-9_-]+$/)) {
+            return false;
+        }
+        return folder.length > 0 && folder.length <= 50;
+    };
+
+    const isHashValid = (hash) => {
+        // if anything rather than a-z, 0-9 return false
+        if (!hash.match(/^[a-zA-Z0-9]+$/)) {
+            return false;
+        }
+        return hash.length === 6;
+    };
+
     // Use onMounted hook to call getUrl when the component is mounted
     onMounted(() => {
-        getUrl();
+        const route = useRoute();
+        const folder = route.params.folder;
+        const hash = route.params.hash;
+
+        if (folder) {
+            console.log('yes, have folder');
+            if (!isFolderValid(folder)) {
+                console.log('Invalid folder name');
+                errorMessage.value = 'Invalid folder name';
+                return;
+            } else {
+                console.log('folder is valid');
+            }
+        }
+
+        if (!isHashValid(hash)) {
+            errorMessage.value = 'Invalid hash';
+            return;
+        }
+
+        getUrl(folder, hash);
     });
 </script>
